7b1792c177d9485e4c3af0a6e80464e7adce9d7a
Aniket <aniketdhokane2071@gmail.com>
2021-12-12 22:37:51 +0530

    let , const and var

---
+++ js/tut3.js
@@ -0,0 +1,69 @@
+// Variables in Javascript
+
+
+// var , let , const   there are three variables in Javascript
+
+var Name = "Aniket";
+
+var channel;
+channel = "Code with Aniket";
+var marks=34;
+
+console.log(Name,channel,marks);
+
+// Rules for creating JS variables
+
+// 1) Cannot start with numbers   
+// 2) can start with letter, numbers or _ , $
+// 3) Are case sensitive     
+
+
+
+var city = 'Delhi';
+// console.log(city);
+
+const ownersName = 'Aniket Pravin Dhokane'; // cannot change the value afterward by using const
+console.log(ownersName);
+
+// let 
+
+// In modern Js we use const and let 
+// let is at block level scope   
+{
+    let city='Aurangabad';
+    console.log(city);
+}
+console.log(city);
+
+
+const  arr1=[2,23,34,45,64,34];
+arr1.push(45);
+console.log(arr1);
+
+
+/* 
+1)camelCase
+2)kebeb-case
+3)snake_case
+4)PasccalCase
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
