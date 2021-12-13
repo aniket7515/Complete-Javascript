c92237b6aa77267ec893cf4d08fc10a039ee1363
Aniket <aniketdhokane2071@gmail.com>
2021-12-13 21:57:29 +0530

    Data types in Javascript

---
+++ js/tut4.js
@@ -0,0 +1,50 @@
+// Primitve data types are
+
+// String 
+var Name = "Aniket";
+console.log("My string is "+Name);
+console.log("My data type is "+ (typeof Name));
+
+
+// Numbers
+
+let marks=34;
+console.log("My data tyoe is "+(typeof marks));
+
+// Boolean
+let isDriver=true;
+console.log("Data type os "+(typeof isDriver));
+
+// undefined
+
+let undef=undefined;
+console.log("  "+(typeof undef))
+
+
+// Arrays
+let myarr=[1,2,3,4, false , "String"];
+console.log("Data type is "+(typeof myarr));
+
+// Object Literals
+
+let Marks={
+    harry:89,
+    Shubham:36,
+    Aniket:100
+
+
+}
+console.log("Data type "+(typeof Marks));
+console.log(Marks);
+
+// Functions
+
+function findName(){
+
+}
+console.log("Data type "+(typeof findName));
+
+
+// Date 
+let date= new Date();
+console.log(typeof date);
