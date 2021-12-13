b307fafbfc3de193351a7e55629c472446747674
Aniket <aniketdhokane2071@gmail.com>
2021-12-13 22:28:34 +0530

    Conversion and Coercion

---
+++ js/tut5.js
@@ -0,0 +1,19 @@
+// type conversion and type coercion  
+
+console.log("Welcome to tut 5");
+let myVar;
+
+myVar=34;
+console.log(myVar);
+
+let booleanVar= String(true);
+
+let d1ate= String(new Date());
+
+let arr= String([1,2,3,,5,6]);
+
+let i=new Date();
+console.log(i.toString());
+
+let stri="23424";
+console.log(stri , (typeof stri));
