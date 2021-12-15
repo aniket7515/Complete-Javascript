d849165af806e4c05cf66d0afbcbccdef1e80080
Aniket <aniketdhokane2071@gmail.com>
2021-12-15 17:32:20 +0530

    understanding DOM

---
+++ js/tut12.js
@@ -0,0 +1,20 @@
+console.log("Welcome to tut12 . js");
+
+let a= document;
+a=document.all;
+// a=document.body;
+// a=document.forms;
+
+// console.log(a);
+
+// Array.from(a).forEach(function(element){
+//     console.log(element);
+// })
+
+a=document.links;
+a=document.links[0].href;
+console.log(a);
+
+
+
+// Array.from(a)  used to make a Array 
